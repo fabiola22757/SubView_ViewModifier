8245b0c373815fca223a246b65ade427a8598a52
Fabiola <fabiola.juarez>
2025-09-19 17:56:32 -0600

    Initial Commit

---
+++ Oreo/OreoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OreoApp.swift
+//  Oreo
+//
+//  Created by win603 on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct OreoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
